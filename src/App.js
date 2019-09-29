002159bc0bb878501031fba5d8f6257628378641
Saulin <roman.saulin@accenture.com>
2019-09-29 14:00:16 +0300

    moved to Gmaps

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-//https://studio.mapbox.com/
-// https://www.pika.dev/packages/@urbica/react-map-gl
-
-//https://github.com/uber/react-map-gl/blob/5.0-release/examples/interaction/src/marker-style.js
-//очень хороший стиль у этой карты
 import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup, GeolocateControl } from "react-map-gl";
-import * as parkDate from "./data/skateboard-parks.json";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import * as parkData from "./data/skateboard-parks.json";
+import mapStyles from "./mapStyles";
 
-
-const geolocateStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  margin: 10
-}
-
-function App() {
-  const [viewport, setViewport] = useState({
-    latitude: 55.7558,
-    longitude: 37.6173,
-    width: "100vw",
-    height: "100vh",
-    zoom: 10
-  })
+function Map() {
   const [selectedPark, setSelectedPark] = useState(null);
 
-  useEffect(() => { //для использования клавиатуры
+  useEffect(() => {
     const listener = e => {
       if (e.key === "Escape") {
         setSelectedPark(null);
@@ -39,57 +26,59 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken='pk.eyJ1Ijoic2F1bGluIiwiYSI6ImNrMHQ4YXdnNzA1cHozY3FwcXducngzNGQifQ.I1n0Ecna664hDQ4PSh5d-w'
-        //mapStyle = 'mapbox://styles/saulin/ck0t9espm0f6p1cqzdug9il7d'
-        onViewportChange = {viewport=>{
-          setViewport(viewport) //для обновления крты. Присваиваем новое состояние
-        }}
-      >
-      <GeolocateControl
-          style={geolocateStyle}
-          positionOptions={{enableHighAccuracy: true}}
-          trackUserLocation={true}
-          fitBoundsOptions = {{maxZoom:7}}
-          showUserLocation = {true}
-          trackUserLocationend = {false}
-      />
-        {parkDate.features.map(park => (
-          <Marker 
-            key={park.properties.PARK_ID}
-            latitude = {park.geometry.coordinates[1]}
-            longitude = {park.geometry.coordinates[0]}
-          >
-            <button 
-              className = 'marker-btn'
-              onClick = {e => {
-                e.preventDefault()
-                setSelectedPark(park)
-              }} 
-            >
-              <img src = '/idea.svg' alt='x'/>
-            </button>
-          </Marker>
-        ))}
-        {selectedPark ? (//если парк выбран, то .., иначе null
-          <Popup //отображlает всплывающие окна
-            latitude = {selectedPark.geometry.coordinates[1]}
-            longitude = {selectedPark.geometry.coordinates[0]}
-            onClose = {() => {
-              setSelectedPark(null)
-            }}
-          >
-            <div>
-              <h2>{selectedPark.properties.NAME}</h2>
-              <p>{selectedPark.properties.DESCRIPTIO}</p>
-            </div>
-          </Popup>
-        ):null }
-      </ReactMapGL>
-    </div>
-  )
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 55.7558, lng: 37.6173 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {parkData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0]
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+          icon={{
+            url: `/bag.svg`,
+            scaledSize: new window.google.maps.Size(25, 25)
+          }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0]
+          }}
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
 
-export default App
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCKAN6fTGh5E7IBMDHULe-JQkJJcIeM0_Q`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
